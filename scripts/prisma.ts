2b0d494f95923e0b0db42d43bc83ffebcd9ca4d4
1vers1on <invers1on1@outlook.com>
2026-01-28 09:26:33 -0500

    i hate commiting just to update the server

--- scripts/prisma.ts
+++ scripts/prisma.ts
@@ -5,18 +5,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const createPrismaClient = () => {
-    const adapter = new PrismaMariaDb({
-        host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT || '3306'),
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        connectionLimit: 5
-    });
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
     return new PrismaClient({
-        adapter,
-        log: []
+        adapter
     });
 };
 
--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -7,16 +7,7 @@ declare global {
 }
 
 const createPrismaClient = () => {
-    const adapter = new PrismaMariaDb({
-        host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT || '3306'),
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        connectionLimit: 5,
-        ssl: false,
-        allowPublicKeyRetrieval: true
-    });
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
     return new PrismaClient({
         adapter,
